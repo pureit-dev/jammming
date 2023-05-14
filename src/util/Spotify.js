a7853911343d1835f25ba3bebe5a10fb4206ce0d
Keith Gibson <keithgibson@Keiths-MacBook-Air.local>
2023-05-14 17:13:29 +0100

    creataed Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,19 +1,36 @@
 const userAccessToken = '';
+const expiresIn = 0;
 const CLIENT_ID = '8bcaf42cbc634ef6a451478d4644f470';
 const REDIRECT_URI = 'http://localhost:3000/';
-const REGEX_ACCESSTOKEN = '/access_token=([^&]*)/';
-const REGEX_EXPIRY = '/expires_in=([^&]*)/';
+const REGEX = '/access_token=([^&]*)&.*expires_in=([^&]*)/';
+const url = window.location.href;
 
 const Spotify = {
 	getAccesToken: () => {
 		if (userAccessToken) {
 			return userAccessToken;
-		} else if (!userAccessToken) {
-			const url = window.location.hrefmatch(REGEX_ACCESSTOKEN && REGEX_EXPIRY)
-			
-		} else {
+		} else if (!userAccessToken && url.match(REGEX).length === 3) {
+			const matches = url.match(REGEX);
+			userAccessToken = matches[1];
+			expiresIn = matches[2];
+			window.setTimeout(() => (userAccessToken = ''), expiresIn * 1000);
+			window.history.pushState('Access Token', null, '/');
+		} else if (
+			!userAccessToken &&
+			url.match(regex)[1] !== userAccessToken
+		) {
 			window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
 		}
+	},
+	search: (term) => {
+		fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+			method: 'GET',
+			headers: { Authorization: `Bearer ${userAccessToken}` },
+		})
+		.then(response => response.json())
+		.then(data => data.map(result => {
+			
+		}))
 	},
 };
 
